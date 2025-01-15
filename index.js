6355e5a3e34ac6ee43d76872231894ff38248a19
Mr-X-01 <lord.akixiro@gmail.com>
2025-01-15 14:26:49 +0300

    New

--- index.js
+++ index.js
@@ -64,4 +64,34 @@ document.addEventListener('DOMContentLoaded', () => {
         // Здесь можно добавить логику отправки формы
         alert('Сообщение отправлено!');
     });
+
+    const typedTextSpan = document.querySelector(".typed-text");
+    const texts = ["Frontend разработчик", "UI/UX дизайнер", "Web Developer"];
+    let textIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+        if (charIndex < texts[textIndex].length) {
+            typedTextSpan.textContent += texts[textIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(erase, 2000);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            typedTextSpan.textContent = texts[textIndex].substring(0, charIndex-1);
+            charIndex--;
+            setTimeout(erase, 50);
+        } else {
+            textIndex = (textIndex + 1) % texts.length;
+            setTimeout(type, 500);
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        setTimeout(type, 1000);
+    });
 });
